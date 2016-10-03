19e877f3be7ec27475eb168884a95221b24bf509
Oliver Veits <oliver.veits@web.de>
2016-10-03 22:07:00 +0200

    Added Internet GW and Route table
    Now functional with OpenShift Master and Nodes in separate VPC, Subnet and
    Security rules.

--- ec2/main.tf
+++ ec2/main.tf
@@ -9,12 +9,27 @@ variable "node_instance_type" {default = "c3.large"}
 variable "aws_availability_zone" { default = "eu-central-1a" }
 variable "aws_region" { default = "eu-central-1" }
 variable "ebs_root_block_size" {default = "50"}
+
 # fedora 25:
 #variable "aws_ami" {default = "ami-a6a15dc9" }
 # CentOS 7:
-variable "aws_ami" {default = "ami-9bf712f4" }
+#variable "aws_ami" {default = "ami-9bf712f4" }
+
+# pre-installed master and nodes (does not work yet):
+#variable "aws_ami_master" {default = "ami-1819e577" }
+#variable "aws_ami_node" {default = "ami-1b19e574" }
+
+# CentOS 7:
+variable "aws_ami_master" {default = "ami-9bf712f4" }
+variable "aws_ami_node" {default = "ami-9bf712f4" }
 variable "ssh_user" {default = "centos"}
-variable "inline_script" {default = "echo hallo"}
+
+#variable "inline_script" {default = "echo hallo"}
+# for CentOS:
+variable "inline_script" {default = "sudo yum install -y python && returnvalue=$? && echo yum | grep -q dnf && sudo yum install -y python2-dnf || exit $returnvalue"}
+# for Fedora:
+#variable "inline_script" {default = "sudo dnf install -y python && returnvalue=$? && echo dnf | grep -q dnf && sudo dnf install -y python2-dnf || exit $returnvalue"}
+
 variable "num_nodes" { default = "2" }
 variable "aws_vpc_openshift_cidr_block" {default = "10.50.0.0/16"}
 variable "aws_subnet_openshift_master_nodes_cidr_block" {default = "10.50.1.0/24"}
@@ -22,6 +37,8 @@ variable "aws_subnet_openshift_master_nodes_cidr_block" {default = "10.50.1.0/24
 
 resource "aws_vpc" "openshift" {
     cidr_block = "${var.aws_vpc_openshift_cidr_block}"
+    enable_dns_hostnames = "true"
+    
 
     tags {
         Name = "openshift"
@@ -30,6 +47,7 @@ resource "aws_vpc" "openshift" {
 
 resource "aws_security_group" "openshift" {
   name = "openshift"
+  vpc_id = "${aws_vpc.openshift.id}"
   description = "Allow all internal traffic, allow all traffic from creator"
 
   ingress {
@@ -54,14 +72,48 @@ resource "aws_security_group" "openshift" {
   }
 }
 
+/*
+    Public Subnet
+*/
+
 resource "aws_subnet" "openshift_master_nodes" {
     vpc_id = "${aws_vpc.openshift.id}"
     cidr_block = "${var.aws_subnet_openshift_master_nodes_cidr_block}"
+    availability_zone = "${var.aws_availability_zone}"
+    map_public_ip_on_launch = "true"
 
     tags {
         Name = "OpenShift Master Nodes"
     }
 }
+
+resource "aws_route_table" "openshift_master_nodes" {
+    vpc_id = "${aws_vpc.openshift.id}"
+
+    route {
+        cidr_block = "0.0.0.0/0"
+        gateway_id = "${aws_internet_gateway.openshift.id}"
+    }
+
+    tags {
+        Name = "Public Subnet for OpenShift"
+    }
+}
+
+resource "aws_route_table_association" "openshift_master_nodes" {
+    subnet_id = "${aws_subnet.openshift_master_nodes.id}"
+    route_table_id = "${aws_route_table.openshift_master_nodes.id}"
+}
+
+resource "aws_internet_gateway" "openshift" {
+    vpc_id = "${aws_vpc.openshift.id}"
+
+    tags {
+        Name = "openshift"
+    }
+}
+/*
+*/
 
 provider "aws" {
     access_key = "${var.aws_access_key}"
@@ -69,11 +121,13 @@ provider "aws" {
     region = "${var.aws_region}"
 }
 
+
 resource "aws_instance" "ose-master" {
-    ami = "${var.aws_ami}"
+    ami = "${var.aws_ami_master}"
     instance_type = "${var.master_instance_type}"
-    security_groups = [ "${aws_security_group.openshift.id}" ]
-    availability_zone = "${var.aws_availability_zone}"
+    vpc_security_group_ids = [ "${aws_security_group.openshift.id}" ]
+#    security_groups = [ "${aws_security_group.openshift.id}" ]
+#    availability_zone = "${var.aws_availability_zone}"
     key_name = "${var.keypair}"
     subnet_id = "${aws_subnet.openshift_master_nodes.id}"
 
@@ -100,10 +154,11 @@ resource "aws_instance" "ose-master" {
 
 resource "aws_instance" "ose-node" {
     count = "${var.num_nodes}"
-    ami = "${var.aws_ami}"
+    ami = "${var.aws_ami_node}"
     instance_type = "${var.node_instance_type}"
-    security_groups = [ "${aws_security_group.openshift.id}" ]
-    availability_zone = "${var.aws_availability_zone}"
+    vpc_security_group_ids = [ "${aws_security_group.openshift.id}" ]
+#    security_groups = [ "${aws_security_group.openshift.id}" ]
+#    availability_zone = "${var.aws_availability_zone}"
     key_name = "${var.keypair}"
     subnet_id = "${aws_subnet.openshift_master_nodes.id}"
 
@@ -127,3 +182,6 @@ resource "aws_instance" "ose-node" {
         }
     }
 }
+/*
+*/
+
